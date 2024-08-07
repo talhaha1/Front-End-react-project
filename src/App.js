1555b4675bcdbfbef58b9feb54ea9fa329735b52
Amaz Ahmed <l226837@lhr.nu.edu.pk>
2024-08-07 20:42:51 +0500

    Describe your changes here

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ import OurServices from './Components/OurServices/OurServices';
  
 import './App.css';
 import Header from "./Components/Header/Header"
-import Footer from "./Components/Footer/Footer"
+import Footer from "./Components/Footer/footer"
 import Stats from "./Components/Stats/Stats"
 import Whoweare from "./Components/Whoweare/whoweare"
- import OurServices from "./Components/OurServics/OurServics"
 import Testimonials from "./Components/Testimonials/Testimonials"
 function App() {
   return (
     <div className="App">
 
 
-      {/* <Header /> */}
+      <Header />
       <Stats />
       <Whoweare />
-      {/* <Footer /> */}
+      
       <OurServices />
-      {/* <Testimonials /> */}
-
+      <Testimonials />
+<Footer />
     </div>
   );
 }
