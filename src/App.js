4063ee68e0f5bd90d90107639eb9e81c2915f616
afeefaKhaliq <afeefakhaliq045@gmail.com>
2024-08-07 08:24:50 -0700

    digitalMarketing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './Components/NavBar/nav'
 import Footer from './Components/Footer/footer'
 import Services from './Components/Services/services'
+import DigitalMarketing from './Components/DigitalMarketing/digitalMarketing';
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Services/>
+      <DigitalMarketing/>
       <Footer/>
     </div>
   );
---
+++ src/Components/DigitalMarketing/digitalMarketing.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './digitalMarketing.css';
+import seo from '../../assests/seo.jpg';
+import email from '../../assests/Email-Marketing.jpg';
+import web from '../../assests/webDesign.png';
+import content from '../../assests/contentWriting.jpg';
+
+const DigitalMarketing = () => {
+  return (
+    <div className="digital-marketing-container">
+      <div className="services-section">
+        <h1 className="services-title">Services</h1>
+        <p className="services-intro">
+          <em>Are your digital marketing goals driven by data, or just a shot in the dark? Either way, ANALYZINN can help to gain competitive advantage by providing you the right knowledge, at the right time, so you can:</em>
+        </p>
+        <ul className="services-list">
+          <li>Understand your customers & offer Superior Customer Experience</li>
+          <li>Identify opportunities and threats & Market Better</li>
+          <li>Respond to business needs proactively</li>
+          <li>Innovate faster with relevant data</li>
+          <li>Gain competitive advantage & make informed and optimized decision</li>
+        </ul>
+      </div>
+      <div className="image-section">
+        <div className="center-image-container">
+          <img src={content} alt="Content Writing" className="center-image" />
+          <p>Content Writing</p>
+        </div>
+        <div className="bottom-images">
+          <div className="bottom-image">
+            <img src={seo} alt="SEO (Search Engine Optimization)" />
+            <p>SEO (Search Engine Optimization)</p>
+          </div>
+          <div className="bottom-image">
+            <img src={email} alt="Email Marketing" />
+            <p>Email Marketing</p>
+          </div>
+          <div className="bottom-image">
+            <img src={web} alt="Website/Logo Design, Landing Pages" />
+            <p>Website/Logo Design, Landing Pages</p>
+          </div>
+        </div>
+      </div>
+      <div className="services-section">
+        <h3 className="services-title">How can you access our Digital Marketing Strategies?</h3>
+        <p className="services-intro">
+          <em>Analyzinn will provide you the additional stack for accessing our Digital Marketing Strategies:</em>
+        </p>
+        <ul className="services-lists">
+          <li>Marketing Analytics Dashboards</li>
+          <li>Revenue Dashboards</li>
+          <li>Customer Level Dashboards</li>
+          <li>Campaign Level Dashboards</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalMarketing;
